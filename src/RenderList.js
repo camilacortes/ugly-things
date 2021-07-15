291d7f946590c4b532444cf6a71046bc49b90926
unknown <camilacortes0103@gmail.com>
2021-07-14 20:57:27 -0600

    designed everything

---
+++ src/RenderList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class RenderList extends React.Component{
+    state={}
+
+    render(){
+        return(
+            <>
+            <div className="title-container">
+               <h1>Place holder text</h1>
+            </div>
+            <div className="desc-container">
+               <p>Description</p>
+            </div>
+            </>
+        )
+    }
+}
+
+export default RenderList 
